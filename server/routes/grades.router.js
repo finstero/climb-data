c77e5f6c52cb69d55230ec3e4266cc1b92a5c6de
Olivia Finster <olivia.finster@gmail.com>
2021-06-28 14:10:12 -0500

    grades router.get

--- server/routes/grades.router.js
+++ server/routes/grades.router.js
@@ -2,16 +2,22 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
+
+// /api/grades
 router.get('/', (req, res) => {
-  // GET route code here
+    const query = `SELECT * FROM "ysd"`
+
+    pool.query(query)
+    .then(result => {
+        res.send(result.rows);
+    })
+    .catch(error => {
+        console.log('error in grades router get', error);
+        res.sendStatus(500);
+    })
 });
 
-/**
- * POST route template
- */
+
 router.post('/', (req, res) => {
   // POST route code here
 });
