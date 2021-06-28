d830666f950f286dad9a814abc39a012e2bd09c9
Olivia Finster <olivia.finster@gmail.com>
2021-06-28 16:12:23 -0500

    date picker install and working. no post yet.

--- src/components/AddRoute/AddRoute.jsx
+++ src/components/AddRoute/AddRoute.jsx
@@ -1,20 +1,33 @@
 import { useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 // material ui
 import Button from '@material-ui/core/Button';
+import 'date-fns';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+    MuiPickersUtilsProvider,
+    KeyboardTimePicker,
+    KeyboardDatePicker,
+} from '@material-ui/pickers';
 
 function AddRoute() {
 
     const history = useHistory();
     const dispatch = useDispatch();
+    const [selectedDate, setSelectedDate] = useState(new Date('2021-06-18T21:11:54'));
 
     useEffect(() => {
         dispatch({
             type: 'FETCH_GRADES'
         })
     }, []);
+
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+    };
 
     const grades = useSelector(store => store.gradesReducer)
 
@@ -27,6 +40,23 @@ function AddRoute() {
                         <option key={grade.id} value={grade.id}>{grade.grade}</option>
                     ))}
                 </select>
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                    <Grid container justify="space-around">
+                        <KeyboardDatePicker
+                            disableToolbar
+                            variant="inline"
+                            format="MM/dd/yyyy"
+                            margin="normal"
+                            id="date-picker-inline"
+                            label="Date picker inline"
+                            value={selectedDate}
+                            onChange={handleDateChange}
+                            KeyboardButtonProps={{
+                                'aria-label': 'change date',
+                            }}
+                        />
+                    </Grid>
+                </MuiPickersUtilsProvider>
                 <Button type="submit" variant="contained" color="primary">
                     Done
                 </Button>
