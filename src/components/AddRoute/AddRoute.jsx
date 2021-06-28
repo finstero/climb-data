e22e032b35329a172c4173daabe3a961a3377487
Olivia Finster <olivia.finster@gmail.com>
2021-06-28 15:45:23 -0500

    material ui install

--- src/components/AddRoute/AddRoute.jsx
+++ src/components/AddRoute/AddRoute.jsx
@@ -12,22 +12,19 @@ function AddRoute() {
             type: 'FETCH_GRADES'
         })
     }, []);
-    
+
     const grades = useSelector(store => store.gradesReducer)
 
     return (
         <>
-            <label for="grades">Choose a grade:</label>
-
-            <select name="grades" id="grades">
-                {grades.map(grade => (
-                    <option key={grade.id} value={grade.id}>{grade.grade}</option>
-                ))}
-                {/* <option value="volvo">Volvo</option>
-                <option value="saab">Saab</option>
-                <option value="mercedes">Mercedes</option>
-                <option value="audi">Audi</option> */}
-            </select>
+            <form>
+                <label for="grades">Choose a grade:</label>
+                <select name="grades" id="grades">
+                    {grades.map(grade => (
+                        <option key={grade.id} value={grade.id}>{grade.grade}</option>
+                    ))}
+                </select>
+            </form>
         </>
     )
 }
