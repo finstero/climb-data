f5557e075eae3218f851ef44f57bfb45f2a6fb81
Olivia Finster <olivia.finster@gmail.com>
2021-06-29 17:14:33 -0500

    IndividualRoute component made and skeleton.

---
+++ src/components/IndividualRoute/IndividualRoute.jsx
@@ -0,0 +1,16 @@
+import { useHistory } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
+function IndividualRoute () {
+
+    
+
+
+    return(
+        <>
+            <h2></h2>
+        </>
+    )
+}
+
+export default IndividualRoute;
