b98b736cdf08d48d584fb725f623c927407df107
Olivia Finster <olivia.finster@gmail.com>
2021-07-01 19:48:43 -0500

    graph router get done

--- server/routes/routesGraph.router.js
+++ server/routes/routesGraph.router.js
@@ -4,7 +4,24 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => {
-    
+    console.log('in graph router get');
+    const query = 
+        `SELECT "grades".grade, count("routes") FROM "grades"
+        LEFT JOIN "routes" ON "grades".id = "routes".grades_id
+        LEFT Join "user" ON "user".id = "routes".user_id
+        WHERE "grades".type='ysd'
+        GROUP BY "grades".id
+        ORDER BY "grades"
+        ;`
+
+    pool.query(query)
+    .then(result => {
+        res.send(result.rows);
+    })
+    .catch(error => {
+        console.log('error in router get', error);
+        res.sendStatus(500);
+    });
 });
 
 
